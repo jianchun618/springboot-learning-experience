e970b9fb2e42c46d06b63be5c4f7c172b57078fc
jianchun <jc33618@163.com>
2021-12-12 20:04:38 +0800

    新增远调用模块

---
+++ remote_call/src/test/java/com/fmbank/remote_call/RemoteCallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fmbank.remote_call;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RemoteCallApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
